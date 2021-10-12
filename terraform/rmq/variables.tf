16011075e09ccd6a1f63e411195729b9f6f733ab
Jeff Haineault <jhaineault@Jeffs-MacBook-Pro.local>
2021-10-12 12:05:29 -0700

    initial Terraform files for rmq

---
+++ terraform/rmq/variables.tf
@@ -0,0 +1,10 @@
+variable "name" {
+	description = "name the instance on deploy"
+}
+
+variable "group" {
+	description = "The group name that ansible's dynamic inventory will group"}
+
+variable "profile" {
+	description = "Which profile to use for IAM"
+}
